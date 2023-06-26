46058e8505fe7fa15063189ef8d19cc0c710dedc
Elena Tykhonova <sivak_elena@ukr.net>
2023-06-26 17:58:36 +0300

    home41 is added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const initialCounts = [
+  {
+    id: 1,
+    value: 0,
+    image: '\uD83D\uDE02'
+  },
+  {
+    id: 2,
+    value: 0,
+    image: '\uD83D\uDE0D'
+  },
+  {
+    id: 3,
+    value: 0,
+    image: '\uD83D\uDE2D'
+  },
+  {
+    id: 4,
+    value: 0,
+    image: '\uD83D\uDE31'
+  },
+  {
+    id: 5,
+    value: 0,
+    image: '\uD83D\uDE0E'
+  }
+]
 
 function App() {
+  const [counts, setCounts] = useState(initialCounts);  
+
+  const plus = (id) => {    
+    const updatedCounts = counts.map(elem => elem.id === id ? {...elem, value: elem.value + 1} : elem);
+    setCounts(updatedCounts);    
+  };
+
+  const showResults = () => {
+    let maxNumber = Math.max(...counts.map(elem => elem.value));
+    let maxCount = counts.filter(elem => elem.value === maxNumber);     
+    setCounts([...maxCount]); 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {counts.map(elem => (
+          <li key={elem.id} style={{ listStyle: 'none' }}>
+            <button onClick={() => plus(elem.id)} style={{ marginRight: '10px', border: '0', backgroundColor: 'transparent', fontSize: '20px' }}>{elem.image}</button>
+            {elem.value}
+          </li>
+        ))}
+      </ul>  
+
+      <button onClick={() => showResults()} style={{ marginLeft: '20px' }}>Show results</button>    
     </div>
   );
+
 }
 
 export default App;
+
+
+
+
